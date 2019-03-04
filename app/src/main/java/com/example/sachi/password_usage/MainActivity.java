dbfac92ed9818b408883cb28b68e016dd751dbb3
sachuuu96 <chandrasena.st.21@gmail.com>
2019-03-04 07:37:10 +0530

    android life cycle

---
+++ app/src/main/java/com/example/sachi/password_usage/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.sachi.password_usage;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText pass_word;
+    private Button button_sbm;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        addListnerOnButton();
+    }
+    public void addListnerOnButton()
+    {
+        pass_word = (EditText)findViewById(R.id.editText);
+        button_sbm = (Button)findViewById(R.id.button);
+        button_sbm.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Toast.makeText(
+                        MainActivity.this,pass_word.getText(),Toast.LENGTH_SHORT
+                ).show();
+            }
+        });
+    }
+}
